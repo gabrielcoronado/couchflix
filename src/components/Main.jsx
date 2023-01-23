fb0af7e7cab9f3c112d895c1bdab2b7e683f32b4
Gabriel Coronado <gabrielcoronado@x.local>
2023-01-23 17:50:03 -0500

    third commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,29 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
+import requests from "../Request";
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((res) => {
+      setMovies(res.data.results);
+    });
+  }, []);
 
-  return <div>Main</div>;
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
+  console.log(movie);
+  return (
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <img
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
