07107939ad003dedd04fa97e6ef42d7ff420265f
Gabriel Coronado <gabrielcoronado@x.local>
2023-01-23 16:23:02 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const Main = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {}, []);
+
+  return <div>Main</div>;
+};
+
+export default Main;
