df0df7880569e8fb6ea25951c2e1e79704f55bbd
Gabriel Coronado <gabrielcoronado@x.local>
2023-01-24 10:19:31 -0500

    added homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Request";
 
 const Home = () => {
   return (
     <div>
       <Main />
+      <Row title="UpComing" fetchUrl={requests.requestUpcoming} />
+      <Row title="Popular" fetchUrl={requests.requestPopular} />
+      <Row title="Trending" fetchUrl={requests.requestTrending} />
+      <Row title="Top Rated" fetchUrl={requests.requestTopRated} />
+      <Row title="Horror" fetchUrl={requests.requestHorror} />
     </div>
   );
 };
