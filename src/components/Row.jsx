61b8272f55058677499e20b40ad6fdf29e476a92
Gabriel Coronado <gabrielcoronado@x.local>
2023-01-24 10:54:14 -0500

    Revert "added firebase"
    This reverts commit 453189be5e1f8582aea00071d40deaf3f077b851.

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import axios from "axios";
-import Movie from "./ Movie";
+import React, { useEffect, useState } from "react";
 
-const Row = ({ title, fetchUrl, rowId }) => {
+const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -11,39 +9,28 @@ const Row = ({ title, fetchUrl, rowId }) => {
       setMovies(res.data.results);
     });
   }, [fetchUrl]);
-
-  const slideLeft = () => {
-    let slider = document.getElementById("slider" + rowId);
-    slider.scrollLeft = slider.scrollLeft - 500;
-  };
-
-  const slideRight = () => {
-    let slider = document.getElementById("slider" + rowId);
-    slider.scrollLeft = slider.scrollLeft + 500;
-  };
-
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center group">
-        <MdChevronLeft
-          onClick={slideLeft}
-          className="text-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
-          size={40}
-        />
-        <div
-          id={"slider" + rowId}
-          className="w-full h-full overflow-x-scroll whitespace-nowrap scrollbar-hide scroll-smooth relative"
-        >
+      <div className="relative flex items-center">
+        <div id={"slider"}>
           {movies?.map((movie, i) => (
-            <Movie movie={movie} key={i} />
+            <div
+              key={i}
+              className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
+            >
+              <img
+                className="w-full h-auto block"
+                src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
+                alt={movie?.title}
+              />
+              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white"></div>
+              <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+                {movie?.title}
+              </p>
+            </div>
           ))}
         </div>
-        <MdChevronRight
-          onClick={slideRight}
-          className="text-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
-          size={40}
-        />
       </div>
     </>
   );
